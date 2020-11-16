997e9f2d268eefa63048447fddf692814adccf81
Tuomas Tervamäki <46777008+tuomaster@users.noreply.github.com>
2020-11-16 13:20:51 +0200

    T1

--- viikko3/tehtavat1-3/nhlreader/src/main/java/ohtu/Player.java
+++ viikko3/tehtavat1-3/nhlreader/src/main/java/ohtu/Player.java
@@ -3,6 +3,12 @@ package ohtu;
 
 public class Player {
     private String name;
+    private String nationality;
+    private int assists;
+    private int goals;
+    private int penalties;
+    private String team;
+    private int games;
 
     public void setName(String name) {
         this.name = name;
@@ -12,9 +18,57 @@ public class Player {
         return name;
     }
 
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public int getAssists() {
+        return assists;
+    }
+
+    public void setAssists(int assists) {
+        this.assists = assists;
+    }
+
+    public int getGoals() {
+        return goals;
+    }
+
+    public void setGoals(int goals) {
+        this.goals = goals;
+    }
+
+    public int getPenalties() {
+        return penalties;
+    }
+
+    public void setPenalties(int penalties) {
+        this.penalties = penalties;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public void setTeam(String team) {
+        this.team = team;
+    }
+
+    public int getGames() {
+        return games;
+    }
+
+    public void setGames(int games) {
+        this.games = games;
+    }
+
     @Override
     public String toString() {
-        return name;
+        return name + " team " + team + " goals " + goals + " assists " + assists;
     }
       
 }
