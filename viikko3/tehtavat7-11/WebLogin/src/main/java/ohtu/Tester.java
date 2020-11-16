0eafcde45e00c053d87bb960e6399d6a61837cb0
Tuomas Tervamäki <46777008+tuomaster@users.noreply.github.com>
2020-11-16 22:27:14 +0200

    T8 tehty

--- viikko3/tehtavat7-11/WebLogin/src/main/java/ohtu/Tester.java
+++ viikko3/tehtavat7-11/WebLogin/src/main/java/ohtu/Tester.java
@@ -1,5 +1,6 @@
 package ohtu;
 
+import java.util.Random;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -11,31 +12,64 @@ public class Tester {
         WebDriver driver = new ChromeDriver();
 
         driver.get("http://localhost:4567");
-        
+
         sleep(2);
-        
-        WebElement element = driver.findElement(By.linkText("login"));
+
+        // Failed login: correct username, wrong password
+//        WebElement element = driver.findElement(By.linkText("login"));
+//        element.click();
+//
+//        sleep(2);
+//
+//        element = driver.findElement(By.name("username"));
+//        element.sendKeys("korppari");
+//        element = driver.findElement(By.name("password"));
+//        element.sendKeys("akkep");
+//        element = driver.findElement(By.name("login"));
+//        
+//        sleep(2);
+//        element.submit();
+
+        // Registering new user
+        WebElement element = driver.findElement(By.linkText("register new user"));
         element.click();
 
         sleep(2);
 
+        Random r = new Random();
+
         element = driver.findElement(By.name("username"));
-        element.sendKeys("pekka");
+        element.sendKeys("jonne" + r.nextInt(100000));
         element = driver.findElement(By.name("password"));
-        element.sendKeys("akkep");
-        element = driver.findElement(By.name("login"));
-        
+        element.sendKeys("salainensalis1");
+        element = driver.findElement(By.name("passwordConfirmation"));
+        element.sendKeys("salainensalis1");
+        element = driver.findElement(By.name("signup"));
+
         sleep(2);
+        
         element.submit();
+        
+         sleep(2);
+
+        // Logout after successful registeration
+        element = driver.findElement(By.linkText("continue to application mainpage"));
+        element.click();
+
+        sleep(2);
+
+        element = driver.findElement(By.linkText("logout"));
+        element.click();
 
         sleep(3);
-        
+
         driver.quit();
     }
-    
-    private static void sleep(int n){
-        try{
-            Thread.sleep(n*1000);
-        } catch(Exception e){}
+
+    private static void sleep(int n) {
+        try {
+            Thread.sleep(n * 1000);
+        } catch (Exception e) {
+        }
     }
 }
