e06eadbcc1d0f7bc1180f2885e99a2f0f8c4103c
Tuomas Tervamäki <46777008+tuomaster@users.noreply.github.com>
2020-11-16 16:44:23 +0200

    T4 valmis

--- viikko3/tehtavat4-6/HelloCucumber/src/test/java/ohtu/Stepdefs.java
+++ viikko3/tehtavat4-6/HelloCucumber/src/test/java/ohtu/Stepdefs.java
@@ -1,4 +1,3 @@
-
 package ohtu;
 
 import io.cucumber.java.en.Given;
@@ -7,8 +6,9 @@ import io.cucumber.java.en.Then;
 import static org.junit.Assert.*;
 
 public class Stepdefs {
+
     Counter counter;
-    
+
     @Given("Counter is initialized")
     public void counterIsInitialized() {
         counter = new Counter();
@@ -26,7 +26,12 @@ public class Stepdefs {
 
     @When("it is incremented by {int}")
     public void itIsIncrementedBy(Integer val) {
-         counter.increment(val);       
-    }    
+        counter.increment(val);
+    }
+
+    @When("it is reset")
+    public void itIsReset() {
+        counter.reset();
+    }
 
 }
