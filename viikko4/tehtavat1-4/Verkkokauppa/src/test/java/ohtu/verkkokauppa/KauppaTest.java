383e400b061f7db282261ff3a14b4835fc2f989c
Tuomas Tervamäki <46777008+tuomaster@users.noreply.github.com>
2020-11-23 16:32:17 +0200

    T3 valmis

--- viikko4/tehtavat1-4/Verkkokauppa/src/test/java/ohtu/verkkokauppa/KauppaTest.java
+++ viikko4/tehtavat1-4/Verkkokauppa/src/test/java/ohtu/verkkokauppa/KauppaTest.java
@@ -1,36 +1,107 @@
 package ohtu.verkkokauppa;
 
+import org.junit.Before;
 import org.junit.Test;
 import static org.mockito.Mockito.*;
 
-
 public class KauppaTest {
-    
+
+    Pankki pankki;
+    Viitegeneraattori viite;
+    Varasto varasto;
+    Kauppa kauppa;
+
+    @Before
+    public void setUp() {
+        pankki = mock(Pankki.class);
+        viite = mock(Viitegeneraattori.class);
+        varasto = mock(Varasto.class);
+        kauppa = new Kauppa(varasto, pankki, viite);
+    }
+
     @Test
     public void ostoksenPaatyttyaPankinMetodiaTilisiirtoKutsutaan() {
         // Luodaan ensin mock-oliot
         Pankki pankki = mock(Pankki.class);
-        
+
         Viitegeneraattori viite = mock(Viitegeneraattori.class);
         // Määritellään, että viitegeneraattori palauttaa viitteen 42
         when(viite.uusi()).thenReturn(42);
-        
+
         Varasto varasto = mock(Varasto.class);
         // Määritellään, että tuote numero 1 on maito, jonka hinta on 5 ja saldo 10
         when(varasto.saldo(1)).thenReturn(10);
         when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
-        
+
         // Sitten testattava kauppa
         Kauppa k = new Kauppa(varasto, pankki, viite);
-        
+
         // Tehdään ostokset
         k.aloitaAsiointi();
         k.lisaaKoriin(1);   // ostetaan tuotetta numero 1 eli maitoa
         k.tilimaksu("pekka", "12345");
-        
+
         // Sitten suoritetaan varmistus, että pankin metodia tilisiirto on kutsuttu
         verify(pankki).tilisiirto(anyString(), anyInt(), anyString(), anyString(), anyInt());
         // Toistaiseksi ei välitetty kutsussa käytetyistä parametreista
     }
-    
+
+    @Test
+    public void ostoksenPaatyttyaPankinMetodiaTilisiirtoKutsutaanOikeallaAsiakkaallaTiliNumeroillaJaSummalla() {
+        when(viite.uusi()).thenReturn(42);
+
+        when(varasto.saldo(1)).thenReturn(10);
+        when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
+
+        kauppa.aloitaAsiointi();
+        kauppa.lisaaKoriin(1);
+        kauppa.tilimaksu("pekka", "12345");
+
+        verify(pankki).tilisiirto(eq("pekka"), anyInt(), eq("12345"), eq("33333-44455"), eq(5));
+    }
+
+    @Test
+    public void kahdenEriTuotteenOstoksenPaatyttyaPankinMetodiaTilisiirtoKutsutaanOikeallaAsiakkaallaTilinumeroillaJaSummalla() {
+        when(varasto.saldo(1)).thenReturn(10);
+        when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
+
+        when(varasto.saldo(2)).thenReturn(5);
+        when(varasto.haeTuote(2)).thenReturn(new Tuote(2, "juusto", 10));
+
+        kauppa.aloitaAsiointi();
+        kauppa.lisaaKoriin(1);
+        kauppa.lisaaKoriin(2);
+        kauppa.tilimaksu("pekka", "12345");
+
+        verify(pankki).tilisiirto(eq("pekka"), anyInt(), eq("12345"), eq("33333-44455"), eq(15));
+    }
+
+    @Test
+    public void kahdenSamanTuotteenOstoksenPaatyttyaPankinMetodiaTilisiirtoKutsutaanOikeallaAsiakkaallaTilinumeroillaJaSummalla() {
+        when(varasto.saldo(1)).thenReturn(10);
+        when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
+
+        kauppa.aloitaAsiointi();
+        kauppa.lisaaKoriin(1);
+        kauppa.lisaaKoriin(1);
+        kauppa.tilimaksu("pekka", "12345");
+
+        verify(pankki).tilisiirto(eq("pekka"), anyInt(), eq("12345"), eq("33333-44455"), eq(10));
+    }
+
+    @Test
+    public void ostoksenPaatyttyaPankinMetodiaTilisiirtoKutsutaanOikeallaAsiakkaallaTilinumerollaJaSummallaKunLisataanVarastostaLoytyvaTuoteJaLoppuOlevaTuote() {
+        when(varasto.saldo(1)).thenReturn(10);
+        when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
+
+        when(varasto.saldo(2)).thenReturn(0);
+        when(varasto.haeTuote(2)).thenReturn(new Tuote(2, "juusto", 10));
+
+        kauppa.aloitaAsiointi();
+        kauppa.lisaaKoriin(1);
+        kauppa.lisaaKoriin(2);
+        kauppa.tilimaksu("pekka", "12345");
+
+        verify(pankki).tilisiirto(eq("pekka"), anyInt(), eq("12345"), eq("33333-44455"), eq(5));
+    }
 }
